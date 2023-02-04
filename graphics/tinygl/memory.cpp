7ecc9a6ddaa8c6c9d9b245a28d11d9ae608dbd37
Cameron Cawley <ccawley2011@gmail.com>
2023-02-04 13:37:12 +0000

    WIP

--- graphics/tinygl/memory.cpp
+++ graphics/tinygl/memory.cpp
@@ -28,25 +28,89 @@
 // Memory allocator for TinyGL
 
 #include "graphics/tinygl/zgl.h"
+#include "common/util.h"
 
 namespace TinyGL {
 
 // modify these functions so that they suit your needs
 
+static size_t max = 0;
+static size_t total = 0;
+
+void log_size(const char *prefix, int size) {
+#if 1
+	Common::String sizeUnits, totalUnits, maxUnits;
+	Common::String sizeStr = Common::getHumanReadableBytes(size, sizeUnits);
+	Common::String totalStr = Common::getHumanReadableBytes(total, totalUnits);
+	Common::String maxStr = Common::getHumanReadableBytes(max, maxUnits);
+	warning("%s %s %s, %s %s total, %s %s max",
+	        prefix, sizeStr.c_str(), sizeUnits.c_str(),
+	        totalStr.c_str(), totalUnits.c_str(),
+	        maxStr.c_str(), maxUnits.c_str());
+#endif
+}
+
 void gl_free(void *p) {
-	free(p);
+	if (!p)
+		return;
+
+	int *buf = ((int *)p) - 1;
+	int size = *buf;
+	free(buf);
+
+	total -= size;
+	log_size("Freed", size);
 }
 
 void *gl_malloc(int size) {
-	return malloc(size);
+	int *buf = (int *)malloc(size + sizeof(int));
+	if (!buf)
+		return NULL;
+	*buf = size;
+
+	total += size;
+	if (max < total)
+		max = total;
+
+	log_size("Allocated", size);
+
+	return buf + 1;
 }
 
 void *gl_zalloc(int size) {
-	return calloc(1, size);
+	int *buf = (int *)calloc(1, size + sizeof(int));
+	if (!buf)
+		return NULL;
+	*buf = size;
+
+	total += size;
+	if (max < total)
+		max = total;
+
+	log_size("Allocated", size);
+
+	return buf + 1;
 }
 
 void *gl_realloc(void *p, int size) {
-	return realloc(p, size);
+	int *buf = ((int *)p) - 1;
+	int old_size = *buf;
+
+	int *new_buf = (int *)realloc(buf, size + sizeof(int));
+	if (!new_buf)
+		return NULL;
+	*new_buf = size;
+
+	total += size - old_size;
+	if (max < total)
+		max = total;
+
+	if (size > old_size)
+		log_size("Increased allocation by", size - old_size);
+	else
+		log_size("Decreased allocation by", old_size - size);
+
+	return new_buf + 1;
 }
 
 } // end of namespace TinyGL
